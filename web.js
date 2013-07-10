641cf2a5b888cbcc33fbdf664f031ac72c3a8b39
Fermiy <fermer@bk.ru>
2013-07-10 10:33:10 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+buf = new Buffer(256);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    buf = new Buffer(256);
+    buf = fs.readFileSync('index.html');
+    response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
